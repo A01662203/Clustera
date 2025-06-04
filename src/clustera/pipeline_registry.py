d64940f810f3a29b494ae4ce771adbfe143dc028
Erandi Ramírez <A01656031@itesm.mx>
2025-06-04 15:45:57 -0600

    TDA remove

--- src/clustera/pipeline_registry.py
+++ src/clustera/pipeline_registry.py
@@ -3,8 +3,6 @@ from kedro.pipeline import Pipeline
 # Importa los pipelines
 from clustera.pipelines.data_science import pipeline as data_science_pipeline
 from clustera.pipelines.data_processing import pipeline as data_processing_pipeline
-from clustera.pipelines.DS_tda_hoy import pipeline as ds_tda_hoy_pipeline
-from clustera.pipelines.DS_tda_lleg import pipeline as ds_tda_lleg_pipeline
 from clustera.pipelines.anticipation_table import pipeline as anticipation_table_pipeline
 from clustera.pipelines.Anom_hoy import pipeline as Anom_hoy_pipeline
 from clustera.pipelines.Anom_lleg import pipeline as Anom_lleg_pipeline
@@ -13,11 +11,9 @@ from clustera.pipelines.Anom_lleg import pipeline as Anom_lleg_pipeline
 def register_pipelines() -> dict[str, Pipeline]:
     return {
         "__default__": data_science_pipeline.create_pipeline() + anticipation_table_pipeline.create_pipeline(),
-        "data_science": data_science_pipeline.create_pipeline() + ds_tda_hoy_pipeline.create_pipeline() + ds_tda_lleg_pipeline.create_pipeline() + Anom_hoy_pipeline.create_pipeline() + Anom_lleg_pipeline.create_pipeline(),
+        "data_science": data_science_pipeline.create_pipeline()  + Anom_hoy_pipeline.create_pipeline() + Anom_lleg_pipeline.create_pipeline(),
         "data_processing": data_processing_pipeline.create_pipeline(),
         "anticipation_table": anticipation_table_pipeline.create_pipeline(),
-        "ds_tda_hoy": ds_tda_hoy_pipeline.create_pipeline(),
-        "ds_tda_lleg": ds_tda_lleg_pipeline.create_pipeline(),
         "anomaly_detection_hoy": Anom_hoy_pipeline.create_pipeline(),
         "anomaly_detection_lleg": Anom_lleg_pipeline.create_pipeline(),
     }
