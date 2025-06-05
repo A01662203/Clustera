ec477a2f625eba0444a871edd0c33d2aa1a09f66
Luisaml39 <A01704173@tec.mx>
2025-06-05 03:27:48 -0600

    Added nombre_estado a tabla anticipacion desglosada

--- src/clustera/pipelines/anticipation_table/nodes.py
+++ src/clustera/pipelines/anticipation_table/nodes.py
@@ -39,13 +39,43 @@ def calcular_tabla_anticipacion(df: pd.DataFrame) -> pd.DataFrame:
     # Expandir lista de IDs
     tabla_expandida = tabla_resumen.explode("lista_ids").rename(columns={"lista_ids": "ID_Reserva"})
 
-    # Merge para recuperar columnas adicionales (solo de esas reservas)
+    # Merge para recuperar columnas adicionales
     columnas_extra = ["ID_Reserva", "Fecha_hoy", "h_num_adu_cat", "hay_menores", "h_num_noc", "h_tfa_total"]
     tabla_expandida = tabla_expandida.merge(df[columnas_extra], on="ID_Reserva", how="left")
 
-    # Reordenar columnas 
+    # Diccionario de claves a nombres de estado
+    diccionario_estados = {
+        "EAGU": "AGUASCALIENTES",
+        "ECA": "CALIFORNIA",
+        "ECH": "CHIHUAHUA",
+        "ECRI": "COSTA RICA",
+        "EDF": "DISTRITO FEDERAL",
+        "EFL": "FLORIDA",
+        "EGA": "GEORGIA",
+        "EGR": "GUERRERO",
+        "EGT": "GUANAJUATO",
+        "EIL": "ILLINOIS",
+        "EJA": "JALISCO",
+        "EMC": "MICHOACÁN",
+        "EMN": "MINNESOTA",
+        "EMR": "MORELOS",
+        "EMX": "MÉXICO",
+        "ENL": "NUEVO LEÓN",
+        "EON": "ONTARIO",
+        "EPB": "PUEBLA",
+        "EQE": "QUERÉTARO",
+        "EQR": "QUINTANA ROO",
+        "ESL": "SAN LUIS POTOSÍ",
+        "ETX": "TEXAS",
+        "AZU": "Azul"
+    }
+
+    # Limpiar espacios en Estado_cve antes de mapear
+    tabla_expandida["nombre_estado"] = tabla_expandida["Estado_cve"].str.strip().map(diccionario_estados)
+
+    # Reordenar columnas
     columnas_principales = [
-        "ID_Reserva", "Fecha_hoy", "Estado_cve", "Tipo_Habitacion_Nombre",
+        "ID_Reserva", "Fecha_hoy", "Estado_cve", "nombre_estado", "Tipo_Habitacion_Nombre",
         "meses_anticipacion", "año_llegada", "mes_llegada", "num_sem_llegada",
         "año_rsv", "mes_rsv", "num_sem_rsv", "h_num_noc_cat", "conteo",
         "max_tfa_total", "min_tfa_total", "avg_tfa_total", "mda_tfa_total",
@@ -53,7 +83,9 @@ def calcular_tabla_anticipacion(df: pd.DataFrame) -> pd.DataFrame:
     ]
     tabla_expandida = tabla_expandida[columnas_principales]
 
+    # Renombrar si la columna venía mal nombrada (opcional)
     tabla_expandida = tabla_expandida.rename(columns={"h_num_rgo": "h_num_noc_cat"})
 
+    # Exportar
     tabla_expandida.to_csv("data/03_primary/tabla_desglosada_anticipacion.csv", index=False)
     return tabla_expandida
